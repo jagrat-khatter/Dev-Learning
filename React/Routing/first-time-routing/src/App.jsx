c5ced4553d30d96c7277ed5d86a702c6a7e7759f
jagrat-khatter <jagratkhatter@gmail.com>
2025-05-19 16:22:49 +0000

    19-05-25

---
+++ React/Routing/first-time-routing/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import {BrowserRouter , Routes , Route , Link , useNavigate} from 'react-router-dom'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+    
+
+    <BrowserRouter>
+    <div><Link to="/">Allen</Link> || <Link to="/neet/class11">Class 11</Link> || <Link to="/neet/class12">Class 12 </Link> </div>
+    <Routes>
+      <Route path="/neet/class11" element={<Class11Program/>} />
+      <Route path="/neet/class12" element={<Class12Program/>} />
+      <Route path="/" element={<LandingPage/>} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+    </BrowserRouter>
+    </div>
+  )
+  function Class11Program(){
+    return <>This is Class 11 Program</>
+  }
+  function Class12Program(){
+    const nav1  = useNavigate(); // useNavigate is a hook function and nav1 is also a function 
+    function HomePage(){
+    nav1("/") ;
+  }
+    return <>This is Class 12 Program
+    <button onClick={HomePage}>Return to Home Page</button></>
+  }
+  function LandingPage(){
+    return <>This is landing page</>
+  }
+  function NotFoundPage(){
+    return <>Content Not Found</>
+  }
+  
+}
+
+export default App
+// Theorey :
+// On using <a href=""></a> syntax itt was redering HTML te whole page again so we we not 
+// benifitting from single page routing 
+// Two components we can use for actual routing are Link and useNaviagate
+// Use useNavigate instead of <Link> when you need to navigate programmatically in
+// response to an event or logic, not just a user clicking a link.
---
+++ React/Routing/layout-1/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import {BrowserRouter , Routes , Route , Link , useNavigate , Outlet} from 'react-router-dom'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+    <BrowserRouter>
+    
+    <Routes>
+      <Route path="/study" element={<Layout />}> 
+        <Route path="study/neet/class11" element={<Class11Program/>} />
+        <Route path="study/neet/class12" element={<Class12Program/>} />
+        <Route path="study/" element={<LandingPage/>} />
+        <Route path="*" element={<NotFoundPage />} />
+     </Route>
+     <Route path="/" element={<div>Hello</div>}/>
+    </Routes>
+    
+    </BrowserRouter>
+    </div>
+  )
+  function Layout(){
+    return <div style={{height : "100vh"}}>
+      <div><Link to="/">Allen</Link> || <Link to="/neet/class11">Class 11</Link> || <Link to="/neet/class12">Class 12 </Link> </div>
+      <Header />
+      <div style={{height : "90vh" , backgrounf : "red"} }> <Outlet /></div>
+      Footer
+    </div>
+  }
+  function Header(){
+    return <div>++++++++++HEADER+++++++++++=</div>
+  }
+  function Class11Program(){
+    return <>This is Class 11 Program</>
+  }
+  function Class12Program(){
+    const nav1  = useNavigate(); // useNavigate is a hook function and nav1 is also a function 
+    function HomePage(){
+    nav1("/") ;
+    }
+    return <>This is Class 12 Program
+    <button onClick={HomePage}>Return to Home Page</button></>
+  }
+  function LandingPage(){
+    return <>This is landing page</>
+  }
+  function NotFoundPage(){
+    return <>Content Not Found</>
+  }
+  
+}
+
+export default App
+// Theorey :
+// On using <a href=""></a> syntax itt was redering HTML te whole page again so we we not 
+// benifitting from single page routing 
+// Two components we can use for actual routing are Link and useNaviagate
+// Use useNavigate instead of <Link> when you need to navigate programmatically in
+// response to an event or logic, not just a user clicking a link.
